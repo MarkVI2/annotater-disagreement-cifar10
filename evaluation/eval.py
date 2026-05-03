3f6b448fefbac768c156f54bf687a66ef320cfdc
AviatorGator <mark.atharv@gmail.com>
2026-05-03 11:23:33 +0530

    Add missing import for pathlib in eval.py

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 import os
 import sys
+from pathlib import Path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from models.cifar_resnet import build_soft_label_model
